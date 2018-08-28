4f586f0d08e345218acad474a6d0049f80b7b6f4
Archie <38021208+archiepv@users.noreply.github.com>
2018-08-27 19:41:14 -0700

    Intro to JS framework (Vue)
    Added a "v-for" in a new script, but it doesn't seem to be showing.

---
+++ scripts/list.js
@@ -0,0 +1,17 @@
+/*---JAVASCRIPT for Vue---*/
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  let app = new Vue({
+    el: '#list',
+    data: {
+      list: [
+        { text: 'Coordinate and initiate increased collaborative interdisciplinary energy research among the universities and the energy industry.t' },
+        { text: 'Assist in the creation and development of a Florida-based energy technology industry.' },
+        { text: 'Provide a state resource for objective energy systems analysis.' },
+        { text: 'Develop education and outreach programs to prepare a qualified energy workforce and informed public.' }
+      ]
+    }
+  })
+
+})
